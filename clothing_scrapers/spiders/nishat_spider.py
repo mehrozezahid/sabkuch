991929fd5d30a633870e046baaee24e1f2ea3123
mehrozezahid <mehroze.zahid@gmail.com>
2015-12-23 20:01:41 +0500

    Editing Nishat to new format, incomplete

--- clothing_scrapers/spiders/nishat_spider.py
+++ clothing_scrapers/spiders/nishat_spider.py
@@ -27,16 +27,22 @@ class NishatSpider(ClothingBaseSpider):
 
         item = Garment()
         item['source_url'] = response.url
-        item['item_category_name'] = self.get_category(sel)
+        item['item_name'] = self.get_item_name(sel)
+        item['item_category_name'] = self.get_category(sel, response)
         item['item_brand_id'] = self.brand_id
         item['item_code'] = self.get_item_code(sel)
         item['item_image_url'] = self.get_image_url(response)
-        item['item_second_image_url'] = self.get_second_image_url(response)
+        item['item_secondary_image_urls'] = self.get_secondary_image_urls(response)
         item['item_description'] = self.get_description(sel)
         item['item_price'] = self.get_price(sel)
-        item['item_is_available'] = self.is_available(sel)
-        item['item_is_on_sale'] = self.is_on_sale(sel)
+        item['item_is_available'] = True
+        item['item_is_on_sale'] = self.is_on_sale(sel, response)
+        item['item_sale_price'] = self.get_sale_price(sel, response)
         yield item
+
+    # for item name (if exists)
+    def get_item_name(self, sel):
+        return None
 
     # for item code
     def get_item_code(self, sel):
@@ -54,22 +60,29 @@ class NishatSpider(ClothingBaseSpider):
         category = sel.xpath(".//*[@id='product']/ul/li/text()").extract()
         if category:
             return re.sub("Product Code: ", '', category[0].strip())
-        return "uncategorized"
+        return "None"
 
     # image url
     def get_image_url(self, response):
         src = response.xpath(".//*[@id='zoom1']/@href")
         return response.urljoin(src[0].extract())
 
-    def get_second_image_url(self, response):
-        src = response.xpath(".//*[@class='cloud-zoom-gallery item']/@href").extract()
-        if src:
-            return response.urljoin(src[0])
+    def get_secondary_image_urls(self, response):
+        images = response.xpath(".//*[@class='cloud-zoom-gallery item']/@href").extract()
+        if len(images) > 1:
+            images.pop()
+            return images
         return None
+
+    # def get_second_image_url(self, response):
+    #     src = response.xpath(".//*[@class='cloud-zoom-gallery item']/@href").extract()
+    #     if src:
+    #         return response.urljoin(src[0])
+    #     return None
 
     # price for product
     def get_price(self, sel):
-        price = sel.xpath("//*[@id='product']//div[@class='price' or @class='special_price']//text()").extract()
+        price = sel.xpath("//*[@id='product']//div[@class='price' or [contains(@style,'line-through')]//text()").extract()
         # removing empty results
         price = filter(None, [s.strip() for s in price])
         if price:
@@ -86,3 +99,9 @@ class NishatSpider(ClothingBaseSpider):
             return False
         sold = sel.xpath("//*[@id='product']//span[contains(.,'sold')]/text()").extract()
         return False if sold else True
+
+    def get_sale_price(self, sel):
+        if self.is_on_sale(sel):
+            sale_price = sel.xpath(".//*[@class='special_price']/text()").extract()[0].strip()
+            return re.sub("Rs ", '', sale_price) if sale_price else None
+        return None
